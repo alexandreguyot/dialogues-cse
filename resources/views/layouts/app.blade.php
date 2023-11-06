2b1a79ef89ba6147700abf7820ad9e868f4e58a9
Guillaume <guillaume@nezumi.fr>
2023-11-06 10:27:29 +0100

    intégration desktop home dark

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,19 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-    <title>{{ __('panel.site_title') }}</title>
-</head>
-
-<body class="bg-backgroundColor antialiased">
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}" /> -->
+    <link rel="stylesheet" href="{{ asset('css/site.css') }}" />
+    <title>{{ __('panel.site_title') }}</title> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Pacifico&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<body class="dark"> 
     <main>
+        <header>
+            <div class="logo"><img src="img/logo.svg"></div>
+            <div class="nav"><a href="#" class="btn green">Demander un devis</a></div>
+        </header>
         @yield('content')
     </main>
 </body>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,6 +1,97 @@
 @extends('layouts.app')
 @section('content')
-    <div class="">
-
+    <div class="content">
+        <div class="hero">
+            <h1>Une solution unique pour votre CSE</h1>
+            <p>Des solutions sur mesure pour une communication impactante</p>
+            <a href="#" class="btn green">Demander un devis</a>
+        </div>
+        <div class="cards-container">
+            <div class="card">
+                <h3>Une identité visuelle qui vous ressemble</h3>
+                <div>
+                    <p>Offrez à votre CSE une identité visuelle distinctive, permettant une identification rapide et aisée par vos collègues. Cette identité …</p>
+                    <a href="#">En savoir plus</a>
+                    <img src="img/illustrations/catalogue_&_rs.png" alt="/catalogue_&_rs" />
+                </div>
+            </div>
+            <div class="card">
+                <h3>Communiquer au quotidien</h3>
+                <div>
+                    <p>Chez Dialogues, nous croyons en l'importance de cultiver un lien quotidien et fort avec vos salariés. Nous proposons…</p>
+                    <a href="#">En savoir plus</a>
+                    <img src="img/illustrations/catalogue_&_rs.png" alt="/catalogue_&_rs" />
+                </div>
+            </div>
+            <div class="card">
+                <h3>Vos réunions CSE</h3>
+                <div>
+                    <p>Les réunions CSE sont des moments cruciaux nécessitant une préparation minutieuse et un suivi rigoureux…</p>
+                    <a href="#">En savoir plus</a>
+                    <img src="img/illustrations/catalogue_&_rs.png" alt="/catalogue_&_rs" />
+                </div>
+            </div>
+            <div class="card">
+                <h3>Une identité visuelle qui vous ressemble</h3>
+                <div>
+                    <p>Un site web bien conçu est essentiel pour une communication efficace. Laissez notre équipe vous guider…</p>
+                    <a href="#">En savoir plus</a>
+                    <img src="img/illustrations/catalogue_&_rs.png" alt="/catalogue_&_rs" />
+                </div>
+            </div>
+        </div>
+        <div class="carousel testimonials">
+            <h2>Ils ont dialogué avec nous</h2>
+            <div class="pin-container">
+                <div class="pin">
+                    <img src="img/illustrations/face_1.png" class="face">
+                    <p>“J’ai présenté le bilan à mon équipe et comme moi ils ont été emballés, ravis… Cela nous représente tellement bien ! Les images, les accroches… Alors un grand merci pour ce joli travail.”</p>
+                    <span class="name">Katell<br>Secrétaire de CSE</span>
+                </div>
+                <div class="pin">
+                    <img src="img/illustrations/face_1.png" class="face">
+                    <p>“Dialogues CSE nous accompagne dans la préparation des réunions avec la direction et dans la réalisation des PV.  L'expertise juridique et l'expérience des CSE ont été déterminants dans notre choix de travailler avec vous.“</p>
+                    <span class="name">Olivier<br>Secrétaire de CSE</span>
+                </div>
+                <div class="pin">
+                    <img src="img/illustrations/face_1.png" class="face">
+                    <p>“Merci pour votre travail ! Après les élections, nous avions envie de faire connaître le CSE avec des idées originales et c’est mission réussie. Nous avons proposé une cocotte pour présenter le CSE, retour en enfance garanti !”</p>
+                    <span class="name">Rosa<br>Membre de CSE</span>
+                </div>
+                <div class="pin">
+                    <img src="img/illustrations/face_1.png" class="face">
+                    <p>“Merci encore de nous avoir proposé des solutions en accord avec nos valeurs. Nous avons pu mettre en place une campagne de communication et d'affichages avec les illustrations originales de Léonie, les salariés ont beaucoup apprécié.”</p>
+                    <span class="name">Anissa<br>Responsable communication de CSE</span>
+                </div>
+            </div>
+        </div>
+        <div class="carousel support">
+            <h2>Nos engagements</h2>
+            <div class="slot-container">
+                <div class="slot">
+                    <img src="img/illustrations/catalogue_&_rs.png">
+                    <h4>Une communication qui vous ressemble</h4>
+                    <p>Nous mettons en œuvre une stratégie de communication adaptée à vos envies et à besoins, en vous associant à chaque étape de notre travail. Nous répondons rapidement à toutes vos demandes pour coller à l’actualité de votre entreprise.</p>
+                </div>
+                <div class="slot">
+                    <img src="img/illustrations/catalogue_&_rs.png">
+                    <h4>Une équipe à l’écoute, experte dans le domaine des CSE</h4>
+                    <p>Vous disposez désormais d’un interlocuteur unique pour l’ensemble de vos besoins dans le domaine de la communication. Nos experts vous proposent des solutions adaptées à vos besoins et vos contraintes. Ils veillent pour vous au respect des règles légales pour communiquer sans inquiétude.</p>
+                </div>
+                <div class="slot">
+                    <img src="img/illustrations/catalogue_&_rs.png">
+                    <h4>Des solutions éco-responsables</h4>
+                    <p>Nous n’avons qu’une planète. Nous nous engageons à proposer des solutions respectueuses de l'environnement. Nous rejoindre, c'est faire le choix d'une communication plus verte.</p>
+                </div>
+            </div>
+        </div>
+        <div class="contact">
+            <h2>On garde contact ? 🤙</h2>
+            <ul>
+                <li><a href="#" class="btn purple">Télécharger notre brochure</a></li>
+                <li><a href="#" class="btn purple">S'inscire à notre newsletter</a></li>
+                <li><a href="#" class="btn green">Demander un devis</a></li>
+            </ul>
+        </div>
     </div>
 @endsection
