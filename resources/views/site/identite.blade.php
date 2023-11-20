ecc2f1852771f64c55ba1121b1beab09bd941646
Alexandre Guyot <alexandre.guyot@wiztivi.com>
2023-11-20 09:43:05 +0100

    Mise a jour

--- resources/views/site/identite.blade.php
+++ resources/views/site/identite.blade.php
@@ -3,22 +3,27 @@
     <div class="content">
         <div class="hero">
             <h1>Une identité visuelle qui vous ressemble</h1>
-            <p>Offrez à votre CSE une identité visuelle distinctive, permettant une identification rapide et aisée par vos collègues. Cette identité reflète vos valeurs et engagements uniques. Elle peut être originale et mémorable, laissant une empreinte marquante. Notre équipe est là pour vous accompagner dans cette démarche en vous proposant :</p>
+            <p>
+                Offrez à votre CSE une identité visuelle distinctive, permettant une identification rapide et aisée par vos collègues. Cette identité reflète vos valeurs et engagements uniques. Elle peut être originale et mémorable, laissant une empreinte marquante.
+                </br>
+                </br>
+                Notre équipe est là pour vous accompagner dans cette démarche en vous proposant :
+            </p>
         </div>
         <div class="carousel support">
             <div class="slot-container">
                 <div class="slot">
-                    <img src="img/illustrations/catalogue_&_rs.png">
+                    <img src="img/illustrations/charte_graphique_cse.png">
                     <h4>Une identité visuelle clé en main</h4>
                     <p>Notre équipe d’experts vous propose de réaliser pour vous une charte graphique propre à votre CSE</p>
                 </div>
                 <div class="slot">
-                    <img src="img/illustrations/catalogue_&_rs.png">
+                    <img src="img/illustrations/outils_de_com.png">
                     <h4>Une communication à votre image</h4>
                     <p>Dialogues CSE vous permet de mettre en œuvre les outils de communication adaptés à votre stratégie et à votre public</p>
                 </div>
                 <div class="slot">
-                    <img src="img/illustrations/catalogue_&_rs.png">
+                    <img src="img/illustrations/goodies.png">
                     <h4>Des goodies qui vous rendront fiers de votre CSE</h4>
                     <p>Dialogues CSE vous propose de créer pour vous des goodies à votre image et éco-responsables</p>
                 </div>
